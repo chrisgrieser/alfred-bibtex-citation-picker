e6872d462b79faff04bf18fcd9a7d51d2b0a4c41
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2021-08-05 19:35:38 +0200

    Add files via upload

--- buffer_outdated_checker.sh
+++ buffer_outdated_checker.sh
@@ -1,10 +1,16 @@
 #!/bin/zsh
 library=~`echo -n $bibtex_library_path | sed -e "s/~//"`
-library_2=~`echo -n $second_library | sed -e "s/~//"`
 buffer="$alfred_workflow_data""/buffer.json"
+l2_modified=false
+
+# this improves performance as otherwise, library_2 will become the home folder and trigger a slower check whether the buffer is newer than the home folder
+if [ "$second_library" != "" ] ; then
+	library_2=~`echo -n $second_library | sed -e "s/~//"`
+	l2_modified=[ "$buffer" -nt "$library_2" ]
+fi
 
 # for whatever reason, [ -e $buffer] does not work, but when buffer is missing, the statement also becomes true, so this works
-if [ "$buffer" -nt "$library" ] && [ "$buffer" -nt "$library_2" ] ; then
+if [ "$buffer" -nt "$library" ] && [ "$l2_modified" = false ] ; then
     echo -n "buffer up-to-date"
 else
     echo -n "buffer outdated or non-existent"
--- buffer_writer.js
+++ buffer_writer.js
@@ -16,7 +16,7 @@ const library_2 = $.getenv('second_library').replace(/^~/, homepath);
 //read bib file &  remove unecessary info to increase speed
 var input = app.doShellScript('cat "' + library + '" | grep -vwE "(abstract|annotate|Bdsk-Url-1|Bdsk-Url-2|date-modified|date-added|keywords|issn|langid|urlyear|isbn|location|pagetotal|series|eprint) =" | grep -vw "%%"');
 if (library_2 != ""){
-    input = input + "\r" + app.doShellScript('cat "' + library_2 + '" | grep -vwE "(abstract|annotate|Bdsk-Url-1|Bdsk-Url-2|date-modified|date-added|keywords|issn|langid|urlyear|isbn|location|pagetotal|series|eprint) =" | grep -vw "%%"');
+   input = input + "\r" + app.doShellScript('cat "' + library_2 + '" | grep -vwE "(abstract|annotate|Bdsk-Url-1|Bdsk-Url-2|date-modified|date-added|keywords|issn|langid|urlyear|isbn|location|pagetotal|series|eprint) =" | grep -vw "%%"');
 }
 
 
@@ -26,139 +26,151 @@ const other_chars = ["{\\~n};ñ","{\\'a};á","{\\'e};é","{\\v c};č","\\c{c
 const special_chars = ["{\\~n};ñ","\\&;&",'``;"',"`;'","\\textendash{};—","---;—","--;—"];
 const decode_pair = [...german_chars, ...other_chars, ...special_chars];
 decode_pair.forEach(pair => {
-    let half = pair.split(";");
-    input = input.replaceAll (half[0],half[1]);
+   let half = pair.split(";");
+   input = input.replaceAll (half[0],half[1]);
 });
 
 const input_array = input.split("@");
 var entry_array = [];
 
-
+//extracts content of a BibTeX-field
 function extract (str){
-    str = str.split(" = ")[1];
-    return str.replace (/,$/,"");
+   str = str.split(" = ")[1];
+   return str.replace (/,$/,"");
 }
 
 input_array.forEach(entry => {
-    let properties = entry.split ("\r");
-    let citekey = "";
-    let author = "";
-    let title = "";
-    let year = "";
-    let authormatches = "";
-    let type = "";
-    let editor = "";
-    let url = "";
-    let collection = "";
-    let doi = "";
-    let volume = "";
-    let issue = "";
-    let numberOfEditors = 0;
+   let properties = entry.split ("\r");
+   let citekey = "";
+   let author = "";
+   let title = "";
+   let year = "";
+   let authormatches = "";
+   let titlematch = "";
+   let type = "";
+   let editor = "";
+   let url = "";
+   let collection = "";
+   let doi = "";
+   let volume = "";
+   let issue = "";
+   let numberOfEditors = 0;
 
-    citekey = properties[0].replace (/.*{(.*),/,"$1");
-    type = properties[0].replace (/(.*){.*/,"$1");
+   citekey = properties[0].replace (/.*{(.*),/,"$1");
+   type = properties[0].replace (/(.*){.*/,"$1");
 
-    properties.forEach (property => {
-        property = property.replace (/[\{|\}]/g,"");
-        if (property.includes ("author =")){
-      		author = extract (property);
-    		author = author.replace (/(, [A-Z]).+?(?= and|$)/gm,""); //remove first names
-    		author = author.replaceAll (" and "," & ");
-    		authormatches = author;
-    		author = author.replace (/\&.*\&.*/,"et al."); // insert et al
-      	} else if (property.includes ("editor =")){
-      		editor = extract (property);
-    		editor = editor.replace (/(, [A-Z]).+?(?= and|$)/gm,""); //remove first names
-    		editor = editor.replaceAll (" and "," & ");
-    		authormatches = editor + " " + authormatches;
-    		numberOfEditors = editor.split("&").length;
-    		editor = editor.replace (/\&.*\&.*/,"et al."); // insert et al
-      	} else if (property.match(/\stitle \=/i) != null){
-      		title = extract (property);
+   properties.forEach (property => {
+
+      property = property.replace (/[\{|\}]/g,""); //remove Tex
+
+      if (property.includes ("author =")){
+         author = extract (property);
+         author = author.replace (/(, [A-Z]).+?(?= and|$)/gm,""); //remove first names
+         author = author.replaceAll (" and "," & ");
+         authormatches = author;
+         author = author.replace (/\&.*\&.*/,"et al."); // insert et al
+
+      } else if (property.includes ("editor =")){
+         editor = extract (property);
+         editor = editor.replace (/(, [A-Z]).+?(?= and|$)/gm,""); //remove first names
+         editor = editor.replaceAll (" and "," & ");
+         authormatches = editor + " " + authormatches;
+         numberOfEditors = editor.split("&").length;
+         editor = editor.replace (/\&.*\&.*/,"et al."); // insert et al
+
+   	} else if (property.match(/\stitle \=/i) != null){
+      	title = extract (property);
     		//shorten, if title too long
     		if (title.length > alfred_bar_length){
     			title = title.substring(0, alfred_bar_length);
     			title = title + "...";
     		}
-      	} else if (property.includes ("year =")){
+    		titlematch = title.replaceAll ("-"," "); //better matching by Alfred
+
+      } else if (property.includes ("year =")){
       		year = property.replace (/.*=\s*{?(\d{4}).*/,"$1");
-      	} else if (property.includes ("date =")){
+      } else if (property.includes ("date =")){
             year = property.replace (/.*=\s*{?(\d{4}).*/,"$1");
-        } else if  (property.includes ("doi =")){
+
+      } else if  (property.includes ("doi =")){
       		doi = extract (property);
-      	} else if  (property.includes ("url =")){
+
+      } else if  (property.includes ("url =")){
       		url = extract (property);
-      	} else if  (property.includes ("volume =")){
+
+      } else if  (property.includes ("volume =")){
       		volume = extract (property);
-      	} else if  (property.includes ("number =")){
+
+      } else if  (property.includes ("number =")){
       		issue = "(" + extract (property) + ")";
-      	} else if  (property.match(/\s(journal|booktitle)\s*\=/i) != null){
+
+      } else if  (property.match(/\s(journal|booktitle)\s*\=/i) != null){
     		collection = "    In: " + extract (property);
-      	}
-    });
+      }
+   });
 
-    //if the initial reading of the filecut away the closing bracket, add it
-    let bibtex_entry = "@" + entry;
-    let opening_brackets = (bibtex_entry.match(/\{/g) || []).length;
-    let closing_brackets = (bibtex_entry.match(/\}/g) || []).length;
-    if (opening_brackets > closing_brackets) { bibtex_entry = bibtex_entry + "}" };
+   //if the initial reading of the filecut away the closing bracket, add it
+   let bibtex_entry = "@" + entry;
+   let opening_brackets = (bibtex_entry.match(/\{/g) || []).length;
+   let closing_brackets = (bibtex_entry.match(/\}/g) || []).length;
+   if (opening_brackets > closing_brackets) { bibtex_entry = bibtex_entry + "}" };
 
-    //when no URL, try to use DOI
-    let urlAppendix = "";
-    let URLsubtitle = "⛔️ There is no URL or DOI.";
-    if (url != "") {
-    	URLsubtitle = "⌃: Open URL " + urlIcon;
-    	urlAppendix = "    " + urlIcon;
-    	if (doi != ""){ urlAppendix = urlAppendix + doiIcon };
-    } else if (doi != "") {
-        URLsubtitle = "⌃: Open DOI " + doiIcon;
-        urlAppendix = "    " + doiIcon;
-    }
+   //when no URL, try to use DOI
+   let urlAppendix = "";
+   let URLsubtitle = "⛔️ There is no URL or DOI.";
+   if (url != "") {
+   	URLsubtitle = "⌃: Open URL " + urlIcon;
+   	urlAppendix = "    " + urlIcon;
+   	if (doi != ""){ urlAppendix = urlAppendix + " " + doiIcon };
+   } else if (doi != "") {
+     URLsubtitle = "⌃: Open DOI " + doiIcon;
+     urlAppendix = "    " + doiIcon;
+   }
 
-    //icon selection
-    let type_icon = "";
-    if (type == "article") {
-        type_icon = "article.png";
-        collection = collection + " " + volume + issue;
-    }
-    else if (type == "book") { type_icon = "book.png"}
-    else if (type == "inbook") { type_icon = "book_chapter.png"}
-    else if (type == "incollection") { type_icon = "book_chapter.png"}
-    else if (type == "misc") { type_icon = "manuscript.png"}
-    else if (type == "unpublished") { type_icon = "manuscript.png"}
-    else if (type == "techreport") { type_icon = "technical_report.png"}
-    else if (type == "inproceedings") { type_icon = "conference.png"}
+   //icon selection
+   let type_icon = "";
+   if (type == "article") {
+     type_icon = "article.png";
+     collection = collection + " " + volume + issue;
+   }
+   else if (type == "book") { type_icon = "book.png"}
+   else if (type == "inbook") { type_icon = "book_chapter.png"}
+   else if (type == "incollection") { type_icon = "book_chapter.png"}
+   else if (type == "misc") { type_icon = "manuscript.png"}
+   else if (type == "unpublished") { type_icon = "manuscript.png"}
+   else if (type == "techreport") { type_icon = "technical_report.png"}
+   else if (type == "inproceedings") { type_icon = "conference.png"}
 
-    // determines correct editor-abbreviation
-    if (numberOfEditors > 1) {var editorAbbrev = "(Eds.)"}
-    else {var editorAbbrev = "(Ed.)"}
+   // determines correct editor-abbreviation
+   if (numberOfEditors > 1) {var editorAbbrev = "(Eds.)"}
+   else {var editorAbbrev = "(Ed.)"}
 
-    // displays editor when there are no authors
-    var authoreditor = author + " ";
-    if ((author == "") && (editor != "")) {
-    	authoreditor = editor + " " + editorAbbrev  + " ";
-    } else if ((author == "") && (editor == "")) {
-    	authoreditor = "";
-    }
+   // displays editor when there are no authors
+   var authoreditor = author + " ";
+   if ((author == "") && (editor != "")) {
+   	authoreditor = editor + " " + editorAbbrev  + " ";
+   } else if ((author == "") && (editor == "")) {
+   	authoreditor = "";
+   }
 
-    entry_array.push ({
-        'title': title,
-        'autocomplete': authoreditor,
-        'subtitle': authoreditor + year + collection + urlAppendix,
-    	'match': title + " " + authormatches + " " + year + " " + collection,
-        'arg': "@" + citekey,
-    	'icon': {'path': type_icon },
-    	'uid': citekey,
-    	"text": { "copy": url },
-    	"mods": {
-    	    "ctrl": {
-    	        "valid": (url != ""),
-    	        "arg": url,
-    	        "subtitle": URLsubtitle,
-    	    },
-    	    "fn": {"arg": bibtex_entry }
-    	}
-    })
+   entry_array.push ({
+      'title': title,
+      'autocomplete': authoreditor,
+      'subtitle': authoreditor + year + collection + urlAppendix,
+      'match': titlematch + " " + authormatches + " " + year + " " + collection,
+      'arg': "@" + citekey,
+      'icon': {'path': type_icon },
+      'uid': citekey,
+      "text": { "copy": url },
+      "mods": {
+         "ctrl": {
+            "valid": (url != ""),
+            "arg": url,
+            "subtitle": URLsubtitle,
+         },
+         "fn": {"arg": bibtex_entry }
+      }
+   })
 });
 
 return JSON.stringify({ 'items': entry_array });
