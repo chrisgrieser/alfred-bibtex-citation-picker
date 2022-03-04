2e32494f5a2e8bbb270909f88516404b2f157eeb
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-04 15:19:27 +0100

    csl downloader added

--- scripts/search-install-csl.js
+++ scripts/search-install-csl.js
@@ -1,92 +1,62 @@
 #!/usr/bin/env osascript -l JavaScript
 
 // Imports
-app = Application.currentApplication();
+const app = Application.currentApplication();
 app.includeStandardAdditions = true;
-ObjC.import('stdlib');
+ObjC.import("stdlib");
 
-//get currently installed
-var current_csl = $.getenv('csl_file');
-var local_csl_folder = $.getenv('alfred_preferences') + "/workflows/" + $.getenv('alfred_workflow_uid') + "/citation-styles/";
-var local_csls = app.doShellScript('ls -1t "' + local_csl_folder + '"');
-var local_csl_array = local_csls.split("\r");
-
-// save local csl-folder for easier access in upcoming utility
-Application('com.runningwithcrayons.Alfred').setConfiguration
-   ('local_csl_folder', {
-      toValue: local_csl_folder,
-      inWorkflow: $.getenv('alfred_workflow_bundleid'),
-      exportable: false}
-   );
-
-//get CSLs online
-var csl_list = app.doShellScript('curl -s "https://api.github.com/repos/citation-style-language/styles/git/trees/master?recursive=1" | grep ".csl" | cut -d ' + "'" + '"' + "'" + ' -f 4');
-var online_csl_array = csl_list.split("\r");
-
-// Capitalize Function
 String.prototype.toCapitalCase = function () {
-   let capital = this.replace(/\w\S*/g, function(txt){
-   	return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-   });
-   // replaceAll not needed, as there is no case where one of them appears more than once
-   capital = capital.replace ("Fur ", "für ");
-   capital = capital.replace ("For ", "for ");
-   capital = capital.replace ("A ", "a ");
-   capital = capital.replace ("An ", "an ");
-   capital = capital.replace ("Of ", "of ");
-   capital = capital.replace ("the ", "the ");
-   capital = capital.replace ("And ", "& ");
-   capital = capital.replace ("Und ", "& ");
-   capital = capital.replace ("Ieee ", "IEEE ");
-   capital = capital.replace ("Acm ", "ACM ");
-   capital = capital.replace ("On ", "on ");
-   capital = capital.replace ("Apa ", "APA ");
-   capital = capital.replace ("Doi ", "DOI ");
-   capital = capital.replace ("Koln", "Köln");
-   capital = capital.replace ("Universitat ", "Universität ");
-   capital = capital.replace ("With ", "with ");
-   capital = capital.replace ("No ", "no ");
-   return capital;
+	const capital = this.replace(/\w\S*/g, function(txt) {
+		return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
+	});
+	return capital
+		.replace ("Fur ", "für ")
+		.replace ("For ", "for ")
+		.replace ("A ", "a ")
+		.replace ("An ", "an ")
+		.replace ("Of ", "of ")
+		.replace ("the ", "the ")
+		.replace ("And ", "& ")
+		.replace ("Und ", "& ")
+		.replace ("Ieee ", "IEEE ")
+		.replace ("Acm ", "ACM ")
+		.replace ("On ", "on ")
+		.replace ("Apa ", "APA ")
+		.replace ("Doi ", "DOI ")
+		.replace ("Koln", "Köln")
+		.replace ("Universitat ", "Universität ")
+		.replace ("With ", "with ")
+		.replace ("No ", "no ");
 };
 
-// Build JSON
-let jsonArray = [];
+// get currently installed
+const CSLfolder= $.getenv("csl_folder").replace(/^~/, app.pathTo("home folder"));
 
-// local CSLs
-local_csl_array.forEach(csl => {
-	let title = csl.replace (/-|\.csl/g," ");
-	title = title.toCapitalCase();
-	let prefix = "❇️ ";
-	let sub = "locally available";
-	if (csl == current_csl){
-		prefix = "✅ ";
-		sub = "currently in use"
-	}
-	jsonArray.push({
-		'title': prefix + title,
-		'subtitle': sub,
-		'arg': local_csl_folder + csl,
-		'type': "file:skipcheck",
+const localCSLs = app.doShellScript("ls -t \"" + CSLfolder + "\"")
+	.split("\r");
+
+
+// get CSLs online
+const onlineCSLs = JSON.parse(app.doShellScript("curl -s \"https://api.github.com/repos/citation-style-language/styles/git/trees/master?recursive=1\""))
+	.tree
+	.map(item => item.path)
+	.filter (item => item.endsWith(".csl"))
+	.map(csl => {
+		const filename = csl.replace("dependent/", "");
+		const title = filename.replace (/-|\.csl/g, " ")
+			.toCapitalCase();
+		const url = "https://raw.githubusercontent.com/citation-style-language/styles/master/" + csl;
+		let prefix = "";
+		let sub = "";
+		if (localCSLs.includes (filename)) {
+			prefix = "🔄 ";
+			sub = "Update local .csl with online version ";
+		}
+		return {
+			"title": prefix + title,
+			"subtitle": sub,
+			"arg": url,
+		};
 	});
-});
 
-// online CSLs
-online_csl_array.forEach(csl => {
-	let filename = csl.replace("dependent/","");
-	let title = filename.replace (/-|\.csl/g," ");
-	title = title.toCapitalCase();
-	let url = "https://raw.githubusercontent.com/citation-style-language/styles/master/" + csl;
-	let prefix = "";
-	let sub = "";
-	if (local_csl_array.includes (filename)){
-		prefix = "🔄 ";
-		sub = "Update local .csl with online version ";
-	}
-	jsonArray.push({
-		'title': prefix + title,
-		'subtitle': sub,
-		'arg': url,
-	});
-});
-
-JSON.stringify({ items: jsonArray });
+JSON.stringify({ items: onlineCSLs });
