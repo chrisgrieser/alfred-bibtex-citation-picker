6be532af0f71ba0375a7d20062d93bd38a59094a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-13 21:14:14 +0100

    if literature note is in Obsidian vault, will open in Obsidian

--- README.md
+++ README.md
@@ -56,7 +56,9 @@ Press the hotkey in any text field to launch the citation picker.
 - `⌘fn + ↵`: Delete the entry from the BibTeX library. (⚠️ Experimental – Feature untested with  BiBTeX files created with apps other than BibDesk. Please make backups.)
 
 ### Literature Note Actions
-- `⌘⇧ + ↵`: Open the literature note. If there is no literature note, a new one will be created and then opened.
+- `⌘⇧ + ↵`: Open the literature note.
+	- If the file is in an [Obsidian Vault](https://obsidian.md/), opens the file in Obsidian.
+	- If there is no literature note, a new one will be created and then opened.
 - `⌘ + Y`: Quicklook the literature note, if there is one (requires QLmarkdown or Peek[^1]).
 - *If the entry has a corresponding literature note, a* `📓` *will be shown.*
 - *Both actions look for literature specified in the workflow settings `literature_note_folder`. The files must be named __exactly__ like the citekey (without `@`) and have a `.md` extension.*
---
+++ scripts/open-create-literature-note.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env osascript -l JavaScript
+
+function run (argv) {
+	ObjC.import("stdlib");
+	const app = Application.currentApplication();
+	app.includeStandardAdditions = true;
+
+	const citekey = argv.join("").replace(/^@/, "");
+	const literatureNotePath = $.getenv("literature_note_folder")
+		.replace(/^~/, app.pathTo("home folder"))
+		+ citekey + ".md";
+
+	// Create literature note
+	const literatureNoteExists = Application("Finder").exists(Path(literatureNotePath));
+	if (!literatureNoteExists) {
+		// TODO: template functionality
+		const template = "---\ntags: \naliases:\n---\n\n# " + citekey;
+		app.doShellScript("echo '" + template + "' > '" + literatureNotePath + "'");
+	}
+
+	// open with Obsidian, if in Vault
+	const obsidianJsonFilePath = app.pathTo("home folder") + "/Library/Application Support/obsidian/obsidian.json";
+	const vaults = JSON.parse(app.read(obsidianJsonFilePath)).vaults;
+	const isFileInObsidianVault = Object.values(vaults).some(v => literatureNotePath.startsWith(v.path));
+
+	if (isFileInObsidianVault) {
+		const URI = "obsidian://open?path=" + encodeURIComponent(literatureNotePath);
+		app.openLocation (URI);
+		return;
+	}
+
+	// open with default app, if not in vault
+	app.doShellScript("open '" + literatureNotePath + "'");
+
+}
