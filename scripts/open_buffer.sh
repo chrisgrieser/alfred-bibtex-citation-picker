4c21befe8f52bdf2778759c62895d6a12e731833
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-14 11:49:12 +0100

    bugfix for first run

--- scripts/open_buffer.sh
+++ scripts/open_buffer.sh
@@ -3,6 +3,9 @@
 LIBRARY="${bibtex_library_path/#\~/$HOME}"
 BUFFER="$alfred_workflow_data/buffer.json"
 
+# create folder, if not existing yet (e.g. first run)
+[[ ! -d "$alfred_workflow_data" ]] && mkdir -p "$alfred_workflow_data"
+
 # reload buffer if outdated or force reloaded
 if [[ ! "$BUFFER" -nt "$LIBRARY" ]] || [[ "$buffer_reload" == "true" ]]; then
 	osascript -l JavaScript "./scripts/buffer_writer.js" > "$BUFFER"
