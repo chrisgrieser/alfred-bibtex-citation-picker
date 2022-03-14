1f954d3960f3bdf384033c6193d7421532104032
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-14 11:51:05 +0100

    patch

--- scripts/open_buffer.sh
+++ scripts/open_buffer.sh
@@ -6,7 +6,7 @@ BUFFER="$alfred_workflow_data/buffer.json"
 # create folder, if not existing yet (e.g. first run)
 [[ ! -d "$alfred_workflow_data" ]] && mkdir -p "$alfred_workflow_data"
 
-# reload buffer if outdated or force reloaded
+# reload buffer if outdated or manually requested reload
 if [[ ! "$BUFFER" -nt "$LIBRARY" ]] || [[ "$buffer_reload" == "true" ]]; then
 	osascript -l JavaScript "./scripts/buffer_writer.js" > "$BUFFER"
 fi
