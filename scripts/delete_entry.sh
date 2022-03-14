5d8c6ca8425aec1915c20ef839541579a20fc2ea
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-14 14:24:33 +0100

    fixed entry deletion for other bibtex formatting

--- scripts/delete_entry.sh
+++ scripts/delete_entry.sh
@@ -3,10 +3,8 @@
 CITEKEY="${citekey/@/}"
 LIBRARY="${bibtex_library_path/#\~/$HOME}"
 
-# deletes from a line matching the citekey to the next "}}"
-# (This is tailored for the BibTeX format used by BibDesk)
-# TODO: check how other apps format this.
-sed -i '' "/{$CITEKEY,/,/}}$/d" "$LIBRARY"
+# deletes from a line matching the citekey to the next "}", https://stackoverflow.com/a/14492880
+sed -i '' "/{$CITEKEY,/,/}$/d" "$LIBRARY"
 
 # pass for notication in Alfred
 echo -n "$CITEKEY"
