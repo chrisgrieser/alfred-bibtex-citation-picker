d88611afee30ea7f02299e8a1792d274378c56a7
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-03-17 01:37:48 +0100

    patch

--- scripts/autocomplete-for-obsidian.js
+++ scripts/autocomplete-for-obsidian.js
@@ -8,7 +8,6 @@ const toImport = "./scripts/bibtex-parser.js";
 eval (app.doShellScript('cat "' + toImport + '"'));
 
 const libraryPath = $.getenv("bibtex_library_path").replace(/^~/, app.pathTo("home folder"));
-
 const delimiter = "|"; // https://tadashi-aikawa.github.io/docs-obsidian-various-complements-plugin/4.%20Options/4.6.%20Custom%20dictionary%20complement/%E2%9A%99%EF%B8%8FColumn%20delimiter/
 
 // ----------
@@ -17,13 +16,15 @@ const rawBibtex = app.doShellScript('cat "' + libraryPath + '"');
 const entryArray = bibtexParse(rawBibtex) // eslint-disable-line no-undef
 	.map (entry => {
 		const { citekey, title, author, year } = entry;
-		const content = "[" + citekey + "]";
+		const authorCleaned = author
+			.replaceAll("& ", "")
+			.replaceAll("et al. ", "");
 
 		// https://tadashi-aikawa.github.io/docs-obsidian-various-complements-plugin/5.%20Terms/%F0%9F%93%9ACustom%20dictionaries/
 		const line = [
-			content, // content to paste
+			"[" + citekey + "]", // text to insert
 			title, // description
-			[author, author.toLowerCase(), year].join(" ") // match (alias), using "partial" as matching strategy
+			[authorCleaned, authorCleaned.toLowerCase(), year].join(" ") // match, using "partial" as matching strategy
 		].join(delimiter);
 
 		return line;
