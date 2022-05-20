9c525e66b5751a31d3ef6ca2663e728c6ebf1f0f
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-20 19:00:18 +0200

    added bare citekey formatting option

--- toggle-citation-format.js
+++ toggle-citation-format.js
@@ -66,6 +66,16 @@ function run(argv) {
 			setEnvVar("_format_page_prefix", ", p. ");
 			setEnvVar("_format_page_suffix", "");
 			break;
+		case "bare citekey":
+			setEnvVar("_format_citation_start", "");
+			setEnvVar("_format_citation_end", "");
+			setEnvVar("_format_citekey_delimiter", ", ");
+			setEnvVar("_format_citekey_prefix", "");
+			setEnvVar("_format_citekey_suffix", "");
+			setEnvVar("_format_page_before_citekey", "false");
+			setEnvVar("_format_page_prefix", ", ");
+			setEnvVar("_format_page_suffix", "");
+			break;
 		case "custom":
 			app.openLocation("https://github.com/chrisgrieser/alfred-bibtex-citation-picker/blob/main/README.md#further-format-customization");
 			break;
