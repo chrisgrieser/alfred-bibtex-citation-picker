700b518fd7d1b83b2684a379576572ded11f52bc
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-05-24 21:24:03 +0200

    patch

--- fuzzy-open-pdf.sh
+++ fuzzy-open-pdf.sh
@@ -7,5 +7,5 @@ CITEKEY_ALT=$(echo "$CITEKEY" | sed 's/\([[:digit:]]...\)/_\1/') # try old namin
 cd "$PDF_FOLDER" || return 1
 
 PDF_LIST=$(find . -maxdepth 3 -type f -name "*.pdf")
-FILE_PATH=$(echo "$PDF_LIST" | grep -i "$CITEKEY" || echo "$PDF_LIST" | grep -i "$CITEKEY_ALT")
+FILE_PATH=$(echo "$PDF_LIST" | grep -i "$CITEKEY" | head -n1 || echo "$PDF_LIST" | grep -i "$CITEKEY_ALT" | head -n1)
 open "$FILE_PATH"
