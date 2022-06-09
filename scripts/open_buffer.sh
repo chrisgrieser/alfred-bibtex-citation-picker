ebfb20718b420536e58e1b59f3956dfbc20bb5bc
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-06-09 11:30:00 +0200

    also reload buffer on PDF & litnote change

--- scripts/open_buffer.sh
+++ scripts/open_buffer.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 LIBRARY="${bibtex_library_path/#\~/$HOME}"
+LIT_NOTES_FOLDER="${literature_note_folder/#\~/$HOME}"
+PDF_FOLDER="${pdf_folder/#\~/$HOME}"
+
 BUFFER="$alfred_workflow_data/buffer.json"
 LAST_VERSION_FILE="$alfred_workflow_data/last_version.txt"
 
@@ -11,15 +14,21 @@ LAST_RUN_VERSION=$(head -n1 "$LAST_VERSION_FILE")
 THIS_VERSION="$alfred_workflow_version"
 
 # reload buffer if
-# - outdated
+# - buffer is outdated (compared to library file)
+# - buffer is outdated (compared to literature notes)
+# - buffer is outdated (compared to PDFs)
 # - manually requested reload
 # - new workflow version (to ensure new features/bug fixes take effect)
 if [[ "$LIBRARY" -nt "$BUFFER" ]] \
+	|| [[ "$LIT_NOTES_FOLDER" -nt "$BUFFER" ]] \
+	|| [[ "$PDF_FOLDER" -nt "$BUFFER" ]] \
 	|| [[ "$buffer_reload" == "true" ]] \
 	|| [[ "$LAST_RUN_VERSION" != "$THIS_VERSION" ]] \
 	; then
 		osascript -l JavaScript "./scripts/buffer_writer.js" > "$BUFFER"
 		echo -n "$THIS_VERSION" > "$LAST_VERSION_FILE"
+	osascript -e "beep" # for debugging
 fi
 
+# pass json to Alfred
 cat "$BUFFER"
