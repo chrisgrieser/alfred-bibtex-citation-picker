913eff329fa731ac96e18a6236509979679eca41
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-12-05 13:15:02 +0100

    fix buffer not reloading

--- scripts/open_buffer.sh
+++ scripts/open_buffer.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # # shellcheck disable=2154
-# LIBRARY="${bibtex_library_path/#\~/$HOME}"
+LIBRARY="${bibtex_library_path/#\~/$HOME}"
 LIT_NOTES_FOLDER="${literature_note_folder/#\~/$HOME}"
 PDF_FOLDER="${pdf_folder/#\~/$HOME}"
 
@@ -10,7 +10,7 @@ LAST_VERSION_FILE="$alfred_workflow_data/last_version.txt"
 
 # create folder and last version file, if not existing yet (e.g. first run)
 [[ -d "$alfred_workflow_data" ]] || mkdir -p "$alfred_workflow_data"
-[[ -e "$LAST_VERSION_FILE" ]] || echo "never run" > "$LAST_VERSION_FILE"
+[[ -e "$LAST_VERSION_FILE" ]] || echo "never run" >"$LAST_VERSION_FILE"
 
 LAST_RUN_VERSION=$(head -n1 "$LAST_VERSION_FILE")
 THIS_VERSION="$alfred_workflow_version"
@@ -22,15 +22,15 @@ THIS_VERSION="$alfred_workflow_version"
 # - workflow preferences (potentially changing matching behavior etc)
 # - manually requested reload
 # - new workflow version (to ensure new features/bug fixes take effect)
-if [[ "$LIBRARY" -nt "$BUFFER" ]] \
-	|| [[ "$LIT_NOTES_FOLDER" -nt "$BUFFER" ]] \
-	|| [[ "$PDF_FOLDER" -nt "$BUFFER" ]] \
-	|| [[ "$PREFS" -nt "$BUFFER" ]] \
-	|| [[ "$buffer_reload" == "true" ]] \
-	|| [[ "$LAST_RUN_VERSION" != "$THIS_VERSION" ]] \
+if [[ "$LIBRARY" -nt "$BUFFER" ]] ||
+	[[ "$LIT_NOTES_FOLDER" -nt "$BUFFER" ]] ||
+	[[ "$PDF_FOLDER" -nt "$BUFFER" ]] ||
+	[[ "$PREFS" -nt "$BUFFER" ]] ||
+	[[ "$buffer_reload" == "true" ]] ||
+	[[ "$LAST_RUN_VERSION" != "$THIS_VERSION" ]] \
 	; then
-		osascript -l JavaScript "./scripts/buffer_writer.js" > "$BUFFER"
-		echo -n "$THIS_VERSION" > "$LAST_VERSION_FILE"
+	osascript -l JavaScript "./scripts/buffer_writer.js" >"$BUFFER"
+	echo -n "$THIS_VERSION" >"$LAST_VERSION_FILE"
 fi
 
 # pass json to Alfred
