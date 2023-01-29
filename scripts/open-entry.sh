c064d5c4815de5400ce3a75870417f2de92e6a35
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-01-29 03:45:26 +0100

    chore

--- scripts/open-entry.sh
+++ scripts/open-entry.sh
@@ -9,7 +9,7 @@ LINE_NO=$(grep -n "{$CITEKEY," "$LIBRARY" | head -n1 | cut -d':' -f1)
 if [[ "$open_entries_in" == "BibDesk" ]]; then
 	open "x-bdsk://$CITEKEY"
 elif [[ "$open_entries_in" == "Sublime" ]]; then
-	# using full path makes this work even if `subl` hasn't been added to PATH
+	# using full path ensures this work even if `subl` hasn't been added to PATH
 	"/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" "$LIBRARY:$LINE_NO"
 elif [[ "$open_entries_in" == "VS Code" ]]; then
 	code "$LIBRARY:$LINE_NO"
@@ -18,6 +18,9 @@ elif [[ "$open_entries_in" == "neovim & alacritty" ]]; then
 elif [[ "$open_entries_in" == "Zotero" ]]; then
 	# INFO "bbt:" ensures that the item is opened in the personal library https://github.com/chrisgrieser/alfred-bibtex-citation-picker/issues/20#issuecomment-1406495450
 	open "zotero://select/items/bbt:@$CITEKEY"
+elif [[ "$open_entries_in" == "default" ]]; then
+	# passing line arguments for the few apps that could parse it
+	open "$LIBRARY" --env=LINE=$LINE_NO
 fi
 
 
