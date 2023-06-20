f0e6c29759aa6c6837d912b174df0c0b1555998b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-06-20 15:54:07 +0200

    feat: org mode & bracketed citekey (#34, #31)

--- scripts/toggle-citation-format.js
+++ scripts/toggle-citation-format.js
@@ -1,106 +1,132 @@
 #!/usr/bin/env osascript -l JavaScript
 
-function run(argv) {
-	ObjC.import("stdlib");
-	const app = Application.currentApplication();
-	app.includeStandardAdditions = true;
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
 
-	function setEnvVar (envVar, newValue) {
-		Application("com.runningwithcrayons.Alfred")
-			.setConfiguration(envVar, {
-				toValue: newValue,
-				inWorkflow: $.getenv("alfred_workflow_bundleid"),
-				exportable: true
-			});
-	}
-
-	const citationFormat = argv.join("");
-	switch (citationFormat) {
-		case "pandoc":
-			setEnvVar("_format_citation_start", "[");
-			setEnvVar("_format_citation_end", "]");
-			setEnvVar("_format_citekey_delimiter", "; ");
-			setEnvVar("_format_citekey_prefix", "@");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "false");
-			setEnvVar("_format_page_prefix", ", p. ");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "multi-markdown":
-			setEnvVar("_format_citation_start", "[");
-			setEnvVar("_format_citation_end", "]");
-			setEnvVar("_format_citekey_delimiter", ";");
-			setEnvVar("_format_citekey_prefix", "#");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "true");
-			setEnvVar("_format_page_prefix", "");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "latex":
-			setEnvVar("_format_citation_start", "\\cite{");
-			setEnvVar("_format_citation_end", "}");
-			setEnvVar("_format_citekey_delimiter", ", ");
-			setEnvVar("_format_citekey_prefix", "");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "true");
-			setEnvVar("_format_page_prefix", "[");
-			setEnvVar("_format_page_suffix", "]");
-			break;
-		case "wikilink":
-			setEnvVar("_format_citation_start", "");
-			setEnvVar("_format_citation_end", "");
-			setEnvVar("_format_citekey_delimiter", ", ");
-			setEnvVar("_format_citekey_prefix", "[[");
-			setEnvVar("_format_citekey_suffix", "]]");
-			setEnvVar("_format_page_before_citekey", "false");
-			setEnvVar("_format_page_prefix", "| ");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "tag":
-			setEnvVar("_format_citation_start", "");
-			setEnvVar("_format_citation_end", "");
-			setEnvVar("_format_citekey_delimiter", ", ");
-			setEnvVar("_format_citekey_prefix", "#");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "false");
-			setEnvVar("_format_page_prefix", ", p. ");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "bare citekey":
-			setEnvVar("_format_citation_start", "");
-			setEnvVar("_format_citation_end", "");
-			setEnvVar("_format_citekey_delimiter", ", ");
-			setEnvVar("_format_citekey_prefix", "");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "false");
-			setEnvVar("_format_page_prefix", ", ");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "eta":
-			setEnvVar("_format_citation_start", "{% cite ");
-			setEnvVar("_format_citation_end", " --prefix %}");
-			setEnvVar("_format_citekey_delimiter", "; ");
-			setEnvVar("_format_citekey_prefix", "");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "false");
-			setEnvVar("_format_page_prefix", ", p. ");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "iA Writer":
-			setEnvVar("_format_citation_start", "[");
-			setEnvVar("_format_citation_end", "]");
-			setEnvVar("_format_citekey_delimiter", "; ");
-			setEnvVar("_format_citekey_prefix", "^");
-			setEnvVar("_format_citekey_suffix", "");
-			setEnvVar("_format_page_before_citekey", "false");
-			setEnvVar("_format_page_prefix", ", p. ");
-			setEnvVar("_format_page_suffix", "");
-			break;
-		case "custom":
-			app.openLocation("https://github.com/chrisgrieser/alfred-bibtex-citation-picker/blob/main/README.md#further-format-customization");
-			break;
-	}
-
+/**
+ * @param {string} envVar
+ * @param {string} newValue
+ */
+function setFormat(envVar, newValue) {
+	Application("com.runningwithcrayons.Alfred").setConfiguration("_format_" + envVar, {
+		toValue: newValue,
+		inWorkflow: $.getenv("alfred_workflow_bundleid"),
+		exportable: true,
+	});
 }
 
-
+/** @type {AlfredRun} */
+// rome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
+	const citationFormat = argv[0];
+	switch (citationFormat) {
+		case "pandoc":
+			// https://pandoc.org/MANUAL.html#citation-syntax
+			setFormat("citation_start", "[");
+			setFormat("citation_end", "]");
+			setFormat("citekey_delimiter", "; ");
+			setFormat("citekey_prefix", "@");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", p. ");
+			setFormat("page_suffix", "");
+			break;
+		case "org":
+			// https://orgmode.org/manual/Citations.html
+			setFormat("citation_start", "[cite:");
+			setFormat("citation_end", "]");
+			setFormat("citekey_delimiter", "; ");
+			setFormat("citekey_prefix", "@");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", p. ");
+			setFormat("page_suffix", "");
+			break;
+		case "multi-markdown":
+			setFormat("citation_start", "[");
+			setFormat("citation_end", "]");
+			setFormat("citekey_delimiter", ";");
+			setFormat("citekey_prefix", "#");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "true");
+			setFormat("page_prefix", "");
+			setFormat("page_suffix", "");
+			break;
+		case "latex":
+			setFormat("citation_start", "\\cite{");
+			setFormat("citation_end", "}");
+			setFormat("citekey_delimiter", ", ");
+			setFormat("citekey_prefix", "");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "true");
+			setFormat("page_prefix", "[");
+			setFormat("page_suffix", "]");
+			break;
+		case "wikilink":
+			setFormat("citation_start", "");
+			setFormat("citation_end", "");
+			setFormat("citekey_delimiter", ", ");
+			setFormat("citekey_prefix", "[[");
+			setFormat("citekey_suffix", "]]");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", "| ");
+			setFormat("page_suffix", "");
+			break;
+		case "bracketed citekey":
+			setFormat("citation_start", "(");
+			setFormat("citation_end", ")");
+			setFormat("citekey_delimiter", ", ");
+			setFormat("citekey_prefix", "");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", p. ");
+			setFormat("page_suffix", "");
+			break;
+		case "tag":
+			setFormat("citation_start", "");
+			setFormat("citation_end", "");
+			setFormat("citekey_delimiter", ", ");
+			setFormat("citekey_prefix", "#");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", p. ");
+			setFormat("page_suffix", "");
+			break;
+		case "bare citekey":
+			setFormat("citation_start", "");
+			setFormat("citation_end", "");
+			setFormat("citekey_delimiter", ", ");
+			setFormat("citekey_prefix", "");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", ");
+			setFormat("page_suffix", "");
+			break;
+		case "eta":
+			setFormat("citation_start", "{% cite ");
+			setFormat("citation_end", " --prefix %}");
+			setFormat("citekey_delimiter", "; ");
+			setFormat("citekey_prefix", "");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", p. ");
+			setFormat("page_suffix", "");
+			break;
+		case "iA Writer":
+			setFormat("citation_start", "[");
+			setFormat("citation_end", "]");
+			setFormat("citekey_delimiter", "; ");
+			setFormat("citekey_prefix", "^");
+			setFormat("citekey_suffix", "");
+			setFormat("page_before_citekey", "false");
+			setFormat("page_prefix", ", p. ");
+			setFormat("page_suffix", "");
+			break;
+		default:
+			app.openLocation(
+				"https://github.com/chrisgrieser/alfred-bibtex-citation-picker/blob/main/README.md#further-format-customization",
+			);
+			break;
+	}
+}
