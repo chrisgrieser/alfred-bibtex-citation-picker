680284971ab2a90afaece3ca0c1ffe74dca0389a
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-14 14:09:43 +0200

    perf: bibliography-pasting

---
+++ scripts/paste-full-bibliography.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+export PATH=/usr/local/bin:/opt/homebrew/bin/:$PATH
+
+if ! command -v pandoc &>/dev/null; then
+	echo -n "You need to install pandoc for this feature."
+	return 0
+fi
+
+CSL="./apa-6th-edition.csl"
+CITEKEY="$*"
+# shellcheck disable=SC2154
+LIBRARY="${bibtex_library_path/#\~/$HOME}"
+DUMMYDOC="---
+nocite: '@$CITEKEY'
+---"
+
+reference=$(echo -n "$DUMMYDOC" |
+	pandoc --citeproc --read=markdown --write=plain --wrap=none \
+	--csl="$CSL" --bibliography="$LIBRARY")
+
+echo -n "$reference"
