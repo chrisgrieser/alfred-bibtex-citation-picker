6a238507625f38efe90b23111b637e3c322bd343
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-07-17 12:49:58 +0200

    refactor: entry-add & Google Books API for ISBN

--- scripts/add-new-entry.js
+++ scripts/add-new-entry.js
@@ -38,19 +38,6 @@ function writeToFile(text, filepath) {
 //──────────────────────────────────────────────────────────────────────────────
 
 /**
- * @param {string[]} arr
- * @param {string} property
- */
-function parseBibtexProperty(arr, property) {
-	const allProperties = arr
-		.map((/** @type {string} */ line) => line.trim())
-		.filter((/** @type {string} */ prop) => prop.startsWith(property + " "));
-	if (!allProperties.length) return "";
-	const value = allProperties[0].split("=")[1].replace(/{|}|,$/g, "").trim();
-	return value;
-}
-
-/**
  * @param {string} citekey
  * @param {string} libraryPath
  */
@@ -59,14 +46,14 @@ function ensureUniqueCitekey(citekey, libraryPath) {
 	const citekeyArray = readFile(libraryPath)
 		.split("\n")
 		.filter((line) => line.startsWith("@"))
-		.map((line) => line.split("{")[1].replaceAll(",", ""));
+		.map((line) => line.split("{")[1].slice(0, -1));
 
 	const alphabet = "abcdefghijklmnopqrstuvwxyz";
 	let i = -1;
 	let nextCitekey = citekey;
 	while (citekeyArray.includes(nextCitekey)) {
-		let nextLetter = alphabet[i];
-		if (i === -1) nextLetter = ""; // first loop
+		const firstLoop = i === -1;
+		const nextLetter = firstLoop ? "" : alphabet[i];
 		nextCitekey = citekey + nextLetter;
 		i++;
 		if (i > alphabet.length - 1) break; // in case the citekey is already used 27 times (lol)
@@ -74,26 +61,17 @@ function ensureUniqueCitekey(citekey, libraryPath) {
 	return nextCitekey;
 }
 
-/** @param {string[]} bibtexPropertyArr */
-function generateCitekey(bibtexPropertyArr) {
-	let year = parseBibtexProperty(bibtexPropertyArr, "year");
-	if (!year) year = "N.D.";
+/**
+ * @param {string} authors all authors in one string joined with " and "
+ * @param {number} year
+ */
+function generateCitekey(authors, year) {
+	const yearStr = year ? year.toString() : "NY";
 
-	let authEds;
-	const authors = parseBibtexProperty(bibtexPropertyArr, "author");
-	const editors = parseBibtexProperty(bibtexPropertyArr, "editor");
-	if (authors) authEds = authors;
-	else if (editors) authEds = editors;
-	else authEds = "NoAuthor";
-
-	let authorStr;
 	const lastNameArr = [];
-	const invalidLastName = authEds.match(/,.*,/) && !authEds.includes("and"); // not complying naming standard with and as delimiter
-
-	if (authEds === "NoAuthor") lastNameArr[0] = "NoAuthor";
-	else if (invalidLastName) lastNameArr[0] = "Invalid";
+	if (!authors) lastNameArr.push("NoAuthor");
 	else {
-		authEds
+		authors
 			.split(" and ") // "and" used as delimiter in bibtex for names
 			.forEach((name) => {
 				// doi.org returns "first last", isbn mostly "last, first"
@@ -102,18 +80,14 @@ function generateCitekey(bibtexPropertyArr) {
 				lastNameArr.push(lastName);
 			});
 	}
-	if (lastNameArr.length < 3) authorStr = lastNameArr.join("");
-	else authorStr = lastNameArr[0] + "EtAl";
-
-	// clean up name
-	authorStr = authorStr
+	const authorStr = (lastNameArr.length < 3 ? lastNameArr.join("") : lastNameArr[0] + "EtAl")
 		// strip diacritics https://stackoverflow.com/a/37511463
 		.normalize("NFD")
 		.replace(/[\u0300-\u036f]/g, "")
 		// no hyphens
 		.replaceAll("-", "");
 
-	const citekey = authorStr + year;
+	const citekey = authorStr + yearStr;
 	return citekey;
 }
 
@@ -125,99 +99,141 @@ function run(argv) {
 	const doiRegex = /\b10.\d{4,9}\/[-._;()/:A-Z0-9]+(?=$|[?/ ])/i; // https://www.crossref.org/blog/dois-and-matching-regular-expressions/
 	const isbnRegex = /^[\d-]{9,}$/;
 
-	const input = argv.join("").trim();
+	const input = argv[0].trim();
 	const libraryPath = $.getenv("bibtex_library_path").replace(/^~/, app.pathTo("home folder"));
+
 	//───────────────────────────────────────────────────────────────────────────
 
-	let bibtexEntry;
-	let newCitekey;
-
+	const entry = {};
 	const isDOI = doiRegex.test(input);
 	const isISBN = isbnRegex.test(input);
 	const mode = $.getenv("mode");
 	if (!(isDOI || isISBN || mode === "parse")) return "input invalid";
 
 	// DOI
+	// https://citation.crosscite.org/docs.html
 	if (isDOI) {
 		const doi = input.match(doiRegex);
 		if (!doi) return "DOI invalid";
 		const doiURL = "https://doi.org/" + doi[0];
-		bibtexEntry = app.doShellScript(`curl -sLH "Accept: application/x-bibtex" "${doiURL}"`); // https://citation.crosscite.org/docs.html
-		if (!bibtexEntry.includes("@") || bibtexEntry.toLowerCase().includes("doi not found"))
+		const response = app.doShellScript(
+			`curl -sL -H "Accept: application/vnd.citationstyles.csl+json" "${doiURL}"`,
+		);
+		if (response.startsWith("<!DOCTYPE html>") || response.toLowerCase().includes("doi not found"))
 			return "DOI not found";
+		const data = JSON.parse(response);
 
-		// ISBN
-	} else if (isISBN) {
+		entry.publisher = data.publisher;
+		entry.author = (data.authors || data.author || [])
+			.map((/** @type {{ given: any; family: any; }} */ author) => `${author.given} ${author.family}`)
+			.join(" and ");
+		const published = data["published-print"] || data["published-online"] || data.published || null;
+		entry.year = published ? published["date-parts"][0][0] : "NY";
+		entry.doi = doi[0];
+		entry.url = data.URL || doiURL;
+		entry.type = data.type.replace(/-?journal-?/, ""); // "journal-article" -> "article"
+		entry.title = data.title;
+		if (entry.type === "article") {
+			entry.journal = data["container-title"];
+			entry.number = data.issue;
+			entry.volume = data.volume;
+			entry.pages = data.page;
+		}
+	}
+
+	// ISBN: Google Books
+	// https://developers.google.com/books/docs/v1/using
+	else if (isISBN) {
+		// INFO ebooks.de API not working anymore :( https://www.ebook.de/de/tools/isbn2bibtex?isbn=9781471181979
 		const isbn = input;
-		bibtexEntry = app.doShellScript(`curl -sL "https://www.ebook.de/de/tools/isbn2bibtex?isbn=${isbn}"`);
-		if (!bibtexEntry.includes("@") || bibtexEntry.toLowerCase().includes("Not found"))
-			return "ISBN not found";
+		const response = app.doShellScript(
+			`curl -sL "https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}"`,
+		);
+		if (!response) return "ISBN not found";
+		const fullData = JSON.parse(response);
+		if (fullData.totalItems === 0) return "ISBN not found";
+		const data = fullData.items[0].volumeInfo;
+		const bookAccessible = fullData.items[0].accessInfo.viewability !== "NO_PAGES"
 
-		// parse via anystyle
-	} else if (mode === "parse") {
+		entry.type = "book";
+		entry.year = parseInt(data.publishedDate.split("-")[0]);
+		entry.author = (data.authors || data.author || []).join(" and ");
+		entry.isbn = isbn;
+		entry.publisher = data.publisher;
+		entry.title = data.title;
+		if (data.subtitle) entry.title += ". " + data.subtitle;
+		if (bookAccessible) entry.url = data.previewLink;
+	}
+
+	// anystyle
+	else if (mode === "parse") {
+
+		// validate installation
+		const anystyleInstalled = app.doShellScript("command -v anystyle || true") !== ""
+		if (!anystyleInstalled) return "anystyle not found";
+
 		// INFO anystyle can't read STDIN, so this has to be written to a file
 		// https://github.com/inukshuk/anystyle-cli#anystyle-help-parse
 		const tempPath = $.getenv("alfred_workflow_cache") + "/temp.txt";
 		writeToFile($.getenv("raw_entry"), tempPath);
-		bibtexEntry = app.doShellScript(`anystyle --stdout --format=bib parse "${tempPath}"`);
+		const response = app.doShellScript(`anystyle --stdout --format=csl parse "${tempPath}"`);
+		const data = JSON.parse(response)[0];
+
+		entry.title = data.title;
+		entry.type = data.type.replace(/-?journal-?/, ""); // "journal-article" -> "article"
+		entry.author = (data.authors || data.author || [])
+			.map((/** @type {{ given: any; family: any; }} */ author) => `${author.given} ${author.family}`)
+			.join(" and ");
+		entry.year = parseInt(data.issued);
+		if (entry.type === "article") {
+			entry.journal = data["container-title"];
+			entry.number = data.issue;
+			entry.volume = data.volume;
+			entry.pages = data.page;
+		} else if (entry.type === "incollection") {
+			entry.booktitle = data["container-title"];
+		}
+		
 	}
 
 	//───────────────────────────────────────────────────────────────────────────
 	// INSERT CONTENT TO APPEND
 
-	// cleaning
-	bibtexEntry = bibtexEntry
-		.replace(/^ {2}/gm, "\t") // tab indentation
-		.replace(/^\s*\w+ =/gm, (/** @type {string} */ field) => field.toLowerCase()) // lowercase all keys
-		.replace(/(\tpublisher.*?) ?(?:gmbh|ltd|publications|llc)(}*,)/im, "$1$2") // publisher garbage
-		.replace(/ ?{}/g, "") // leftover from publisher cleanup
-		.replace("\tdate =", "\tyear =") // consistently "year"
-		.replace(/\tyear = \{?(\d{4}).*\}?/g, "\tyear = $1,") // clean year key
-		.replaceAll("%2F", "/") // fix for URL key in some DOIs
-		.replace(/amp\$\\mathsemicolon\$/g, "") // invalid bibtex
-		.replace(/(?!^)\}$/gm, "},"); // add trailing comma to all properties
+	// cleanup
+	if (entry.publisher) entry.publisher = entry.publisher.replace(/gmbh|ltd|publications?|llc/i, "").trim();
+	if (entry.pages) entry.pages = entry.pages.replace(/(\d+)[^\d]+?(\d+)/, "$1--$2"); // double-dash
 
-	// convert to array + remove first/last line (for correct key sorting)
-	let newProps = bibtexEntry.split(/[\n\r]/);
-	const firstLine = newProps.shift();
-	newProps.pop();
+	// Generate citekey & enclosing lines
+	let citekey = generateCitekey(entry.author, entry.year);
+	citekey = ensureUniqueCitekey(citekey, libraryPath);
+	const firstLine = `@${entry.type}{${citekey},`;
+	const keywordsLine = "\tkeywords = {},";
+	const lastLine = "}";
 
-	// remove trash keys & sort keys alphabethically
-	// rome-ignore format: more compact
-	const keysToDelete = [ "ean", "month", "issn", "language", "copyright", "pagetotal", "address", "abstract", "series" ];
-	newProps = [...new Set(newProps)]
-		.filter((prop) => {
-			const key = prop.split("=")[0].trim();
-			if (key === "url" && (prop.includes("doi") || prop.includes("ebook"))) return false;
-			return !keysToDelete.includes(key);
-		})
-		.sort();
-	// remove comma for last element
-	newProps[newProps.length - 1] = newProps.at(-1).slice(0, -1);
-
-	// Generate citekey, enclose entry with first/last line
-	newCitekey = generateCitekey(newProps);
-	newCitekey = ensureUniqueCitekey(newCitekey, libraryPath);
-	newProps.unshift(firstLine.split("{")[0] + "{" + newCitekey + ",");
-	newProps.push("}");
-
-	// Insert keywords field
-	// (only if there is no keyword property already – some DOI providers do add
-	// keyword fields of their own)
-	if (!newProps.some((/** @type {string} */ prop) => prop.includes("keywords =")))
-		newProps.splice(1, 0, "\tkeywords = {},");
+	// create bibtex text from entry object
+	const propertyLines = [];
+	for (const key in entry) {
+		if (key === "type") continue; // already inserted in first line
+		let value = entry[key];
+		// escape bibtex values
+		if (typeof value === "string") value = "{" + value.replace(/([A-Z]{2,})/g, "{$1}") + "}";
+		propertyLines.push(`\t${key} = ${value},`);
+	}
+	propertyLines.sort();
+	// remove comma from last entry
+	propertyLines[propertyLines.length - 1] = propertyLines[propertyLines.length - 1].slice(0, -1);
 
 	// Write result
-	const newEntry = newProps.join("\n");
-	appendToFile(newEntry, libraryPath);
+	const newEntryAsBibTex = [firstLine, keywordsLine, ...propertyLines, lastLine].join("\n");
+	appendToFile(newEntryAsBibTex, libraryPath);
 
 	// Copy Citation
-	const copyCitation = $.getenv("copy_citation_on_adding_entry") === "1"
+	const copyCitation = $.getenv("copy_citation_on_adding_entry") === "1";
 	if (copyCitation) {
-		const pandocCitation = `[@${newCitekey}]`
+		const pandocCitation = `[@${citekey}]`;
 		app.setTheClipboardTo(pandocCitation);
 	}
 
 	delay(0.1); // delay to ensure the file is written
-	return newCitekey; // pass for opening function
+	return citekey; // pass for opening function
 }
--- scripts/validate-anystyle.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-export PATH=/usr/local/lib:/usr/local/bin:/opt/homebrew/bin/:$PATH
-
-if ! command -v anystyle &>/dev/null; then
-	echo "You need to install anystyle for this feature."
-fi
