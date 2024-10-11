3083bb15c577e47010562ec35c762dc26a024408
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-10-11 23:01:26 +0200

    style: lint and clean up some old files

--- scripts/open-create-literature-note.js
+++ scripts/open-create-literature-note.js
@@ -1,38 +1,40 @@
 #!/usr/bin/env osascript -l JavaScript
+ObjC.import("stdlib");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+//───────────────────────────────────────────────────────────────────────────
 
-function run (argv) {
-	ObjC.import("stdlib");
-	const app = Application.currentApplication();
-	app.includeStandardAdditions = true;
-
+/** @type {AlfredRun} */
+// biome-ignore lint/correctness/noUnusedVariables: Alfred run
+function run(argv) {
 	const citekey = argv.join("");
-	const literatureNotePath = $.getenv("literature_note_folder")
-		.replace(/^~/, app.pathTo("home folder"))
-		+ "/" + citekey + ".md";
+	const literatureNotePath =
+		$.getenv("literature_note_folder").replace(/^~/, app.pathTo("home folder")) +
+		`/${citekey}.md`;
 
 	// Create literature note
 	const literatureNoteExists = Application("Finder").exists(Path(literatureNotePath));
 	if (!literatureNoteExists) {
-		// TODO: template functionality
 		const template = "---\ntags: \naliases:\n---\n\n";
 		app.doShellScript(`echo '${template}' > '${literatureNotePath}'`);
 	}
 
 	// open with Obsidian, if in Vault
-	const obsidianJsonFilePath = app.pathTo("home folder") + "/Library/Application Support/obsidian/obsidian.json";
+	const obsidianJsonFilePath =
+		app.pathTo("home folder") + "/Library/Application Support/obsidian/obsidian.json";
 	const vaults = JSON.parse(app.read(obsidianJsonFilePath)).vaults;
-	const isFileInObsidianVault = Object.values(vaults).some(v => literatureNotePath.startsWith(v.path));
+	const isFileInObsidianVault = Object.values(vaults).some((v) =>
+		literatureNotePath.startsWith(v.path),
+	);
 
 	if (isFileInObsidianVault) {
 		Application("Obsidian").activate();
-		// eslint-disable-next-line no-magic-numbers
 		delay(0.3); // needed so Obsidian can pick up newly created file
-		const URI = "obsidian://open?path=" + encodeURIComponent(literatureNotePath);
-		app.openLocation(URI);
+		const uri = "obsidian://open?path=" + encodeURIComponent(literatureNotePath);
+		app.openLocation(uri);
 		return;
 	}
 
 	// open with default app, if not in vault
 	app.doShellScript(`open '${literatureNotePath}'`);
-
 }
--- scripts/open_citation_picker.sh
+++ scripts/open_citation_picker.sh
@@ -7,7 +7,7 @@ last_version_file="$alfred_workflow_data/last_version.txt"
 
 # create folder and last version file, if not existing yet (e.g. first run)
 [[ -d "$alfred_workflow_data" ]] || mkdir -p "$alfred_workflow_data"
-[[ -e "$last_version_file" ]] || echo "never run" >"$last_version_file"
+[[ -e "$last_version_file" ]] || echo "never run" > "$last_version_file"
 
 # RELOAD BUFFER if buffer is outdated compared to…
 # - library or 2nd library file
@@ -24,8 +24,8 @@ if [[ "$bibtex_library_path" -nt "$buffer" ]] ||
 	[[ "$buffer_reload" == "true" ]] ||
 	[[ "$(head -n1 "$last_version_file")" != "$alfred_workflow_version" ]] \
 	; then
-	osascript -l JavaScript "./scripts/write-citation-picker-buffer.js" >"$buffer"
-	echo -n "$alfred_workflow_version" >"$last_version_file"
+	osascript -l JavaScript "./scripts/write-citation-picker-buffer.js" > "$buffer"
+	echo -n "$alfred_workflow_version" > "$last_version_file"
 fi
 
 # pass json to Alfred
