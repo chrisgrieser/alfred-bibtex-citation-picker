9d147d431ea31d9a737299c0a11c94a96a2ecc1b
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2025-10-16 15:51:28 +0200

    refactor: shell script var naming

--- scripts/autofile-pdf.sh
+++ scripts/autofile-pdf.sh
@@ -6,26 +6,26 @@ citekey=$(echo "$*" | tr -d "\n")
 # GET FILEPATH & CHECK VALIDITY OF VARIABLES
 
 # shellcheck disable=2154
-PDF_FOLDER="${pdf_folder/#\~/$HOME}"
-if [[ ! -e "$PDF_FOLDER" ]]; then
+pdf_folder="${pdf_folder/#\~/$HOME}"
+if [[ ! -e "$pdf_folder" ]]; then
 	echo "PDF folder does not exist." && return 1
-elif [[ -z "$PDF_FOLDER" ]]; then
+elif [[ -z "$pdf_folder" ]]; then
 	echo "PDF folder not set." && return 1
 fi
 
 # shellcheck disable=2154
 if [[ "$mode" == "id+autofile" ]]; then
-	SELECTED_FILE="$pdf_filepath" # set via Alfred
+	selected_file="$pdf_filepath" # set via Alfred
 else
-	NO_OF_SELECTIONS=$(osascript -l JavaScript -e 'Application("Finder").selection().length')
-	SELECTED_FILE=$(osascript -l JavaScript -e 'decodeURIComponent(Application("Finder").selection()[0]?.url()).slice(7)')
-	EXT="${SELECTED_FILE##*.}"
+	no_of_selections=$(osascript -l JavaScript -e 'Application("Finder").selection().length')
+	selected_file=$(osascript -l JavaScript -e 'decodeURIComponent(Application("Finder").selection()[0]?.url()).slice(7)')
+	ext="${selected_file##*.}"
 
-	if [[ $NO_OF_SELECTIONS -eq 0 ]]; then
+	if [[ $no_of_selections -eq 0 ]]; then
 		echo "⛔️ No file selected." && return 1
-	elif [[ $NO_OF_SELECTIONS -gt 1 ]]; then
+	elif [[ $no_of_selections -gt 1 ]]; then
 		echo "⛔️ More than one file selected." && return 1
-	elif [[ "$EXT" != "pdf" ]]; then
+	elif [[ "$ext" != "pdf" ]]; then
 		echo "⛔️ Selected file is not a PDF." && return 1
 	fi
 fi
@@ -33,8 +33,8 @@ fi
 #───────────────────────────────────────────────────────────────────────────────
 
 # auto-filing
-AUTHOR=$(echo -n "$citekey" | sed -E 's/[[:digit:]]+.*//') # assumes citekey is author+year
-FIRST_CHARACTER=${citekey:0:1}
+author=$(echo -n "$citekey" | sed -E 's/[[:digit:]]+.*//') # assumes citekey is author+year
+first_character=${citekey:0:1}
 
 # shellcheck disable=2154
 title=$(grep --ignore-case --after-context=20 --max-count=1 "{${citekey}," "${bibtex_library_path}" |
@@ -49,16 +49,16 @@ safe_truncated_title=$(
 		sed -E 's/ $//'
 )
 
-AUTOFILE_FOLDER="$PDF_FOLDER/$FIRST_CHARACTER/$AUTHOR"
-AUTOFILE_PATH="$AUTOFILE_FOLDER/${citekey}_${safe_truncated_title}.pdf"
+autofile_folder="$pdf_folder/$first_character/$author"
+autofile_path="$autofile_folder/${citekey}_${safe_truncated_title}.pdf"
 
-if [[ -e "$AUTOFILE_PATH" ]]; then
+if [[ -e "$autofile_path" ]]; then
 	echo "⛔️ There is already a PDF file."
 	echo "Delete it and run auto-file again."
-	open -R "$AUTOFILE_PATH"
+	open -R "$autofile_path"
 	return 1
 fi
 
-mkdir -p "$AUTOFILE_FOLDER"
-mv "$SELECTED_FILE" "$AUTOFILE_PATH"
-open -R "$AUTOFILE_PATH"
+mkdir -p "$autofile_folder"
+mv "$selected_file" "$autofile_path"
+open -R "$autofile_path"
